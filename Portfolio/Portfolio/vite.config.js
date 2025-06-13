59a18328abadbdc4af0185892cdc6a82474f2c5a
Emmanuelsheyi <esheyi8484@gmail.com>
2025-06-13 16:42:16 +0100

    Add _redirects for Netlify routing

--- Portfolio/Portfolio/vite.config.js
+++ Portfolio/Portfolio/vite.config.js
@@ -1,7 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 important if deploying to sub-path, optional for root
 })
